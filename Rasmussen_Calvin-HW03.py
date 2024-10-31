1272dbc537770097593e62acf9fd54b19e3d3d33
crasmussen727 <crasmu14@uwyo.edu>
2024-10-31 06:07:02 +0000

    IT is done again

--- Rasmussen_Calvin-HW03.py
+++ Rasmussen_Calvin-HW03.py
@@ -1,3 +1,7 @@
+#Name: Calvin Rasmussen 
+#Lab Section: 13
+#Submission Date: 11/05/24
+#Sources, help given to/received from: Notes from class, and notes from designing with devices
 def is_leap_year(year):
     # Check if a given year is a leap year
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
